5d217b84a356a7f73cf19b4c5408bfcc01a4d176
Lin Li Oechsle <tui20361@temple.edu>
2022-02-23 16:51:24 -0500

    created basic theme background and font color

--- bazaara_ui/src/App.js
+++ bazaara_ui/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./GlobalStyles";
+import theme from "./themes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+   return (
+      <ThemeProvider theme={theme}>
+         <>
+            <GlobalStyles />
+            <p>Testing themes</p>
+         </>
+      </ThemeProvider>
+   )
 }
-
 export default App;
---
+++ bazaara_ui/src/themes.js
@@ -0,0 +1,7 @@
+const theme = {
+    background: '#FBE9E8',
+    color: '#EE7064',
+    buttonBg: '#c5718d'
+ }
+ 
+ export default theme;
