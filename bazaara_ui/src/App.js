310821c431c8eab71f3b72d89194ebd23e5e0dcd
Lin Li Oechsle <tui20361@temple.edu>
2022-02-23 17:09:18 -0500

    created basic global theme template

--- bazaara_ui/src/App.js
+++ bazaara_ui/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./GlobalStyles";
-import theme from "./themes";
+import { GlobalStyles } from "./themes/GlobalStyles";
+import theme from "./themes/themes";
 
 const App = () => {
    return (
       <ThemeProvider theme={theme}>
          <>
-            <GlobalStyles />
-            <p>Testing themes</p>
+          <GlobalStyles />
+              <p>BAZAARA</p>
+
+
          </>
       </ThemeProvider>
    )
--- bazaara_ui/src/themes/GlobalStyles.js
+++ bazaara_ui/src/themes/GlobalStyles.js
@@ -2,9 +2,11 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
    body {
-     font-family: "Arial", monospace;
+     font-family: "Roboto", monospace, Helvetica;
+     font-size: 48px;
      background: ${({ theme }) => theme.background};
      color: ${({ theme }) => theme.color};
+     text-align: center;
      transition: all 0.50s linear; 
   }
 `
