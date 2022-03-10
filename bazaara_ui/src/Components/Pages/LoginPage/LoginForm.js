cfb5a8733ef2aa2280bd680289e4d7b84e66005e
Brok3nPix3l <tuj17945@temple.edu>
2022-03-09 21:00:47 -0500

    Started working on login
    Got error message "auth/admin-restricted-operation Firebase: Error (auth/admin-restricted-operation)." Possible that there's something janky going on with LoginForm not being a jsx file

--- bazaara_ui/src/Components/Pages/LoginPage/LoginForm.js
+++ bazaara_ui/src/Components/Pages/LoginPage/LoginForm.js
@@ -2,58 +2,108 @@ import React from 'react';
 
 import './styles.css';
 import { Link } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export class LoginForm extends React.Component {
-    render(){
-    return(
-      <>
-      <section className='login_form'>
+  render() {
+    var username;
+    var password;
 
-        <div id="loginform">
+    return (
+      <>
+        <section className='login_form'>
+
+          <div id="loginform">
             <FormHeader title="Login" />
-            <Form />
+            <UsernameForm
+              description="Username"
+              placeholder="Enter your username"
+              type="text"
+              onChange={username=this.value}
+            />
+            <PasswordForm description="Password"
+              placeholder="Enter your password"
+              type="password"
+              onChange={password=this.value}
+            />
+            <LoginButton onClick={handleFormSubmit(username, password)}/>
             <RegisterButton />
             <OtherMethods />
-        </div>
-      </section>
+          </div>
+        </section>
       </>
     )
-    }
+  }
+}
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCvdgBDgUVeVM_RBBskYiBFjdNsRqkN4Bk',
+  authDomain: 'bazaara-342116.firebaseapp.com'
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const handleFormSubmit = (username, password) => {
+  createUserWithEmailAndPassword(auth, username, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log('ERROR during createUserWithEmailAndPassword: ' + errorCode + ' ' + errorMessage);
+      // ..
+    });
 }
 
 const FormHeader = props => (
-    <h2 id="headerTitle">{props.title}</h2>
+  <h2 id="headerTitle">{props.title}</h2>
 );
 
 
-const Form = props => (
-   <div>
-     <FormInput description="Username" placeholder="Enter your username" type="text" />
-     <FormInput description="Password" placeholder="Enter your password" type="password"/>
-     <FormButton title="Log in"/>
-   </div>
-);
+// const Form = props => (
+//   <div>
+//     <FormInput description="Username" placeholder="Enter your username" type="text" onChange={setUsername({ value })} />
+//     <FormInput description="Password" placeholder="Enter your password" type="password" onChange={setPassword({ value })} />
+//     <FormButton title="Log in" />
+//   </div>
+// );
 
-const FormButton = props => (
+const LoginButton = props => (
   <div id="button" class="row">
-    <button>{props.title}</button>
+    <button>
+      Log in
+    </button>
   </div>
 );
 
-const FormInput = props => (
+const UsernameForm = props => (
   <div class="row">
     <label>{props.description}</label>
-    <input type={props.type} placeholder={props.placeholder}/>
-  </div>  
+    <input type={props.type} placeholder={props.placeholder} onChange={props.onChange} />
+  </div>
+);
+
+const PasswordForm = props => (
+  <div class="row">
+    <label>{props.description}</label>
+    <input type={props.type} placeholder={props.placeholder} onChange={props.onChange} />
+  </div>
 );
 
 const RegisterButton = props => (
-    <div class="switchText">
-        Are you new here?
-        <Link to="/register">
-            Click here to register.
-        </Link>
-    </div>
+  <div class="switchText">
+    Are you new here?
+    <Link to="/register">
+      Click here to register.
+    </Link>
+  </div>
 )
 
 const OtherMethods = props => (
