70648649e041499549998e15a4e0cf85b8ebc76a
Brok3nPix3l <tuj17945@temple.edu>
2022-03-24 00:05:20 -0400

    Login is functional, but still won't navigate to the homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./themes/GlobalStyles";
 import theme from "./themes/themes";
@@ -6,22 +6,112 @@ import Button from "./Components/Button";
 import './App.css';
 import { LoginForm } from './Components/Pages/LoginPage/LoginForm';
 import { RegisterForm } from './Components/Pages/RegisterPage/RegisterForm';
-import  HomeForm  from './Components/Pages/HomePage/HomeForm';
+import HomeForm from './Components/Pages/HomePage/HomeForm';
 import Navbar from './Components/NavBar/Navbar';
 import { ProductSearch } from './Components/Pages/ProductSearch/ProductSearch';
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
 } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 export default function App() {
+  const [usernameErrorMessage, setUsernameErrorMessage] = useState('')
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('')
+
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
+  }
+
+  const app = initializeApp(firebaseConfig)
+
+  const auth = getAuth(app)
+
+  function handleLogin(username, password) {
+    setUsernameErrorMessage('')
+    setPasswordErrorMessage('')
+
+    if (username.length === 0) { 
+      setUsernameErrorMessage('Please enter your email')
+      return false
+    }
+
+    if (password.length === 0) {
+      setPasswordErrorMessage('Please enter your password')
+      return false
+    }
+
+    signInWithEmailAndPassword(auth, username, password)
+      .then((userCredential) => {
+        // Signed in
+        console.log('Login successful. Current user: ' + userCredential.user)
+        // ...
+      })
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/invalid-email':
+            setUsernameErrorMessage('Invalid email')
+            break;
+          case 'auth/wrong-password':
+            setPasswordErrorMessage('Invalid password')
+            break;
+        }
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('Login failed. Error message: ' + errorCode + ' ' + errorMessage)
+        // ..
+        return false
+      })
+      return true
+  }
+
+  function handleRegister(username, password) {
+
+    createUserWithEmailAndPassword(auth, username, password)
+      .then((userCredential) => {
+        // Signed in
+        console.log('Login successful. Current user: ' + userCredential.user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('Login failed. Error message: ' + errorCode + ' ' + errorMessage)
+        // ..
+      })
+  }
+
+  function handleSignout() {
+    signOut(auth).then(() => {
+      // Sign-out successful
+      console.log('Successfully signed out')
+    }).catch((error) => {
+      // An error happened
+      console.log('Failed to signout. Error: ' + error)
+    })
+  }
+
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar handleSignout={handleSignout}/>
       <Routes>
-        <Route index element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
+        <Route index element={<LoginForm
+          handleLogin={handleLogin}
+          usernameErrorMessage={usernameErrorMessage}
+          passwordErrorMessage={passwordErrorMessage}
+          auth={auth}
+        />} />
+        <Route path="/register" element={<RegisterForm
+          handleRegister={handleRegister}
+        />} />
         <Route path="/home" element={<HomeForm />} />
         <Route path="/search" element={<ProductSearch />} />
       </Routes>
--- src/Components/Pages/LoginPage/LoginForm.js
+++ src/Components/Pages/LoginPage/LoginForm.js
@@ -1,95 +1,17 @@
 import React, { useState } from 'react';
 
 import './styles.css';
-import { Link, Navigate } from 'react-router-dom';
-import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Link, useNavigate } from 'react-router-dom';
 
-export function LoginForm() {
+export function LoginForm({ handleLogin, usernameErrorMessage, passwordErrorMessage }) {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
-    measurementId: process.env.REACT_APP_measurementId,
-  }
+  const navigate = useNavigate
 
-  console.log('api key is ' + firebaseConfig.apiKey)
-
-  const app = initializeApp(firebaseConfig)
-
-  const handleFormSubmit = (passedUsername, passedPassword) => {
-
-    const auth = getAuth(app)
-
-    createUserWithEmailAndPassword(auth, passedUsername, passedPassword)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log('Login successful. Current user: ' + user)
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log('Login failed. Error message: ' + errorCode + ' ' + errorMessage)
-        // ..
-      })
-
-    // console.log('Called handleFormSubmit function')
-    // console.log('Username: ' + username + ' Password: ' + password)
-
-    // const errors = {
-    //   username: 'invalid username',
-    //   password: 'invalid password',
-    // }
-
-    // const userData = {
-    //   username: 'user',
-    //   password: 'password',
-    // }
-
-    // if (userData) {
-    //   if (userData.username !== username) {
-    //     setErrorMessages({ name: 'username', message: errors.username })
-    //     console.log('Username ' + password + ' does not match ' + userData.username)
-    //   } else {
-    //     if (userData.password !== password) {
-    //       setErrorMessages({ name: 'password', message: errors.password })
-    //       console.log('Password ' + password + ' does not match ' + userData.password)
-    //     } else {
-    //       setIsSubmitted({ status: true, message: 'Logged in successfully!' })
-    //     }
-    //   }
-    // }
-
-    // console.log('Username: ' + username + ' Password: ' + password)
-    // if (username === 'user' && password === 'password') {
-    //   setFailedToLoginMessage('')
-    //   Navigate('/home')
-    // } else {
-    //   setFailedToLoginMessage('Username or Password is incorrect')
-    // }
-    // createUserWithEmailAndPassword(auth, username, password)
-    //   .then((userCredential) => {
-    //     // Signed in 
-    //     const user = userCredential.user;
-    //     console.log(user);
-    //     // ...
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     console.log('ERROR during createUserWithEmailAndPassword: ' + errorCode + ' ' + errorMessage);
-    //     // ..
-    //   });
+  function handleLoginSubmit() {
+    handleLogin(username, password)
+    navigate("/home")
   }
 
   const handleUsernameChange = event => {
@@ -100,52 +22,9 @@ export function LoginForm() {
     setPassword(event.target.value)
   }
 
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-
-  const renderLoggedInMessage = () => (
-    true === isSubmitted.status && (
-      <div className="loggedIn">{isSubmitted.message}</div>
-    )
-  )
-
-
   const FormHeader = props => (
     <h2 id="headerTitle">{props.title}</h2>
   );
-
-
-  // const Form = props => (
-  //   <div>
-  //     <FormInput description="Username" placeholder="Enter your username" type="text" onChange={setUsername({ value })} />
-  //     <FormInput description="Password" placeholder="Enter your password" type="password" onChange={setPassword({ value })} />
-  //     <FormButton title="Log in" />
-  //   </div>
-  // );
-
-  const LoginButton = props => (
-    <div id="button" className="row">
-      <button>
-        Log in
-      </button>
-    </div>
-  );
-
-  const FormInput = props => (
-    <div className="row">
-      <label>{props.description}</label>
-      <input type={props.type} placeholder={props.placeholder} onChange={props.onChange} value={props.value} />
-    </div>
-  );
-
-  // const PasswordForm = props => (
-  //   <div className="row">
-  //     <label>{props.description}</label>
-  //     <input type={props.type} placeholder={props.placeholder} onChange={props.onChange} value={props.value} />
-  //   </div>
-  // );
 
   const RegisterText = props => (
     <div className="switchText">
@@ -179,48 +58,31 @@ export function LoginForm() {
     <a id="googleIcon"></a>
   );
 
-  const FailedToLoginMessage = props => (
-    <div>
-      {props.message}
-    </div>
-  )
-
   return (
-    <section className='login_form'>
-      <div id="loginform">
-        {renderLoggedInMessage()}
-        <FormHeader title="Login" />
-        <div className="row">
-          <label>Username</label>
-          <input type="text" placeholder="Enter your username" onChange={handleUsernameChange} value={username} required />
-          {renderErrorMessage('username')}
+      <section className='login_form'>
+        <div id="loginform">
+          <FormHeader title="Login" />
+          <div className="row">
+            <label>Username</label>
+            <input type="text" placeholder="Enter your username" onChange={handleUsernameChange} value={username} required />
+            {usernameErrorMessage}
+          </div>
+
+          <div className="row">
+            <label>Password</label>
+            <input type="password" placeholder="Enter your password" onChange={handlePasswordChange} value={password} required />
+            {passwordErrorMessage}
+          </div>
+
+          <div id="button" className="row">
+            <button onClick={() => handleLoginSubmit(username, password)}>
+              Log in
+            </button>
+          </div>
+
+          <RegisterText />
+          <OtherMethods />
         </div>
-        {/* <FormInput description="Username"
-          placeholder="Enter your username"
-          type="text"
-          value={username}
-          onChange={handleUsernameChange}
-        /> */}
-        <div className="row">
-          <label>Password</label>
-          <input type="password" placeholder="Enter your password" onChange={handlePasswordChange} value={password} required />
-          {renderErrorMessage('password')}
-        </div>
-        {/* <FormInput description="Password"
-          placeholder="Enter your password"
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-        /> */}
-        <div id="button" className="row">
-          <button onClick={(username, password) => handleFormSubmit(username, password)}>
-            Log in
-          </button>
-        </div>
-        {/* <LoginButton onClick={handleFormSubmit} /> */}
-        <RegisterText />
-        <OtherMethods />
-      </div>
-    </section>
+      </section>
   )
 }
