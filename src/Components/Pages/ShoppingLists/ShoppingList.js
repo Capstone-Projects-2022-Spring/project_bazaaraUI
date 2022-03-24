15d6761098aea9d49f74012e5feb338a32abfce9
Lin Li Oechsle <tui20361@temple.edu>
2022-03-23 22:24:01 -0400

    added ability to remove products, added list management dropdown, fixed ui, started add product functionality

--- src/Components/Pages/ShoppingLists/ShoppingList.js
+++ src/Components/Pages/ShoppingLists/ShoppingList.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+class Product {
+    constructor(productName, weight, price) {
+        this.productName = productName;
+        this.weight = weight;
+        this.price = price;
+    }
+}
+
+
 class ShoppingList {
-
-
-    constructor(name, products) {
+    constructor (name, productCollection) {
         this.name = name;
-        this.products = products;
+        this.productCollection = productCollection;
+    }
+
+    static addProduct(productName, weight, price) {
+        this.productCollection.push(new Product(productName, weight, price));
     }
 
 }
+
 
 class ShoppingListCollection {
     static collection = [];
@@ -18,8 +30,22 @@ class ShoppingListCollection {
     }
 }
 
-ShoppingListCollection.add('Breakfast', ['apples', 'bread', 'eggs']);
-ShoppingListCollection.add('Dinner', ['potatoes', 'steak', 'ketchup']);
+
+/*
+ShoppingListCollection.add('Breakfast', 
+    [new Product("Sweet Baby Ray's Barbecue Sauce", 29.0, 2.99),
+     new Product("Bananas", 16, 0.49),
+     new Product("OREO Sandwich Cookies Chocolate", 25.5, 5.49)
+    ]);*/
+
+//ShoppingListCollection.add('Dinner', [ShoppingList.addProduct("Sweet Baby Ray's Barbecue Sauce", 29.0, 2.99)]);
+
+ShoppingListCollection.add('Dinner', [new Product("Sweet Baby Ray's Barbecue Sauce", 29.0, 2.99), new Product("Bananas", 16, 0.49), new Product("OREO Sandwich Cookies Chocolate", 25.5, 5.49)]);
+ShoppingListCollection.add('snacks', [new Product("sauce", 29.0, 2.99), new Product("fruit", 16, 0.49), new Product("OREO Sandwich Cookies Chocolate", 25.5, 5.49)]);
 
 
-export {ShoppingList, ShoppingListCollection};
+//alert('list:' + JSON.stringify(ShoppingListCollection.collection[0].productCollection));
+//alert('product name???: ' + ShoppingList.productCollection[0].productName.toString());
+
+
+export {ShoppingList, ShoppingListCollection, Product};
--- src/Components/Pages/ShoppingLists/ShoppingListDisplay.js
+++ src/Components/Pages/ShoppingLists/ShoppingListDisplay.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import './styles.css';
 import { ShoppingList, ShoppingListCollection } from './ShoppingList';
+import Product from './Product'
+import ProductCard from './Product';
+
 
 export class ShoppingListDisplay extends React.Component {
+
     render() {
+
+    
         return (
+
             <p>
                 <h1>{this.props.lists[this.props.displayIndex].name}</h1>
-
+                
                 {
-                    this.props.lists[this.props.displayIndex].products.map((productName) => (
-                        <p>{productName}</p>
-                    ))
-
+                    this.props.lists[this.props.displayIndex].productCollection.map((product, index) => (
+                        <p><ProductCard name={product.productName} weight={product.weight} price={product.price} removeProduct={this.props.removeProduct} clicked={index}/></p>
+                        ))
                 }
             </p>
         );
     }
 }
-
+//this.props.lists[this.props.displayIndex].productCollection
 export default ShoppingListDisplay;
