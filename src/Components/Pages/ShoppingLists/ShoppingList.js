84dcab371ef326e364952181bca74c952e463dd1
Lin Li Oechsle <tui20361@temple.edu>
2022-03-23 22:33:11 -0400

    Revert "added ability to remove products, added list management dropdown, fixed ui, started add product functionality"
    This reverts commit 15d6761098aea9d49f74012e5feb338a32abfce9.

--- src/Components/Pages/ShoppingLists/ShoppingList.js
+++ src/Components/Pages/ShoppingLists/ShoppingList.js
@@ -1,26 +1,14 @@
 import React from 'react';
 
-class Product {
-    constructor(productName, weight, price) {
-        this.productName = productName;
-        this.weight = weight;
-        this.price = price;
-    }
-}
-
-
 class ShoppingList {
-    constructor (name, productCollection) {
-        this.name = name;
-        this.productCollection = productCollection;
-    }
 
-    static addProduct(productName, weight, price) {
-        this.productCollection.push(new Product(productName, weight, price));
+
+    constructor(name, products) {
+        this.name = name;
+        this.products = products;
     }
 
 }
-
 
 class ShoppingListCollection {
     static collection = [];
@@ -30,22 +18,8 @@ class ShoppingListCollection {
     }
 }
 
-
-/*
-ShoppingListCollection.add('Breakfast', 
-    [new Product("Sweet Baby Ray's Barbecue Sauce", 29.0, 2.99),
-     new Product("Bananas", 16, 0.49),
-     new Product("OREO Sandwich Cookies Chocolate", 25.5, 5.49)
-    ]);*/
-
-//ShoppingListCollection.add('Dinner', [ShoppingList.addProduct("Sweet Baby Ray's Barbecue Sauce", 29.0, 2.99)]);
-
-ShoppingListCollection.add('Dinner', [new Product("Sweet Baby Ray's Barbecue Sauce", 29.0, 2.99), new Product("Bananas", 16, 0.49), new Product("OREO Sandwich Cookies Chocolate", 25.5, 5.49)]);
-ShoppingListCollection.add('snacks', [new Product("sauce", 29.0, 2.99), new Product("fruit", 16, 0.49), new Product("OREO Sandwich Cookies Chocolate", 25.5, 5.49)]);
+ShoppingListCollection.add('Breakfast', ['apples', 'bread', 'eggs']);
+ShoppingListCollection.add('Dinner', ['potatoes', 'steak', 'ketchup']);
 
 
-//alert('list:' + JSON.stringify(ShoppingListCollection.collection[0].productCollection));
-//alert('product name???: ' + ShoppingList.productCollection[0].productName.toString());
-
-
-export {ShoppingList, ShoppingListCollection, Product};
+export {ShoppingList, ShoppingListCollection};
