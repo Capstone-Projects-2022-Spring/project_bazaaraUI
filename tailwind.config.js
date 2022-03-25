cc20a09213459559aa2ec89199a36dcdf6075ae6
shivani-07 <tug85474@temple.edu>
2022-03-25 19:05:28 -0400

    pushing entire folder b/c missed some files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        "teko":['Teko', 'sans-serif']
+      },
+      colors:{
+        'primary':'#0D9494'
+      }
+    },
+  },
+  plugins: [],
+}
