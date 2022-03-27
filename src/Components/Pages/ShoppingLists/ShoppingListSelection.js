ba9b398eb38fe5b3e6418026c731a29fda327465
shivani-07 <tug85474@temple.edu>
2022-03-27 10:47:40 -0400

    change background and button rounded

--- src/Components/Pages/ShoppingLists/ShoppingListSelection.js
+++ src/Components/Pages/ShoppingLists/ShoppingListSelection.js
@@ -27,15 +27,15 @@ export class ShoppingListSelection extends React.Component {
     render() {
         return(
 
-            <section className="">
+            <section className="flex flex-col items-start space-y-2 bg-purple-200 justify-start">
                 <h1>Shopping Lists</h1>
-                <button onClick={(e) => this.props.togglePop(e)}>+ Create New List</button>
+                <button onClick={(e) => this.props.togglePop(e)} className="px-2 py-1 text-sm rounded-full text-white bg-purple-600">+ Create New List</button>
                 {this.props.seen ?    
                     <div className="modal">
                         <div className="modal_content">
                             <span className="close"></span>
 
-                                    <input type="text" placeholder="New Shopping List Name" onChange={(e) => this.props.handleInput(e)}/>
+                                    <input type="text" className='px-2 text-sm w-48 mr-0 py-1 rounded-full  bg-gray-100 border-2 border-2 border-purple-300' placeholder="New Shopping List Name" onChange={(e) => this.props.handleInput(e)}/>
                                 <br />
                                 <button onClick={(e) => this.props.handleAddList(this.props.value, e)}>Add</button>
 
